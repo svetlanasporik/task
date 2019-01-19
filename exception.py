68df6ba8cdba6c092a06917cf59e2aa09a465527
svetlanasporik <45627601+svetlanasporik@users.noreply.github.com>
2019-01-19 13:16:37 +0300

    Delete exception.py

--- exception.py
+++
@@ -1,8 +0,0 @@
-try:
-    foo()
-except ZeroDivisionError:
-    print ("ZeroDivisionError")
-except ArithmeticError:
-    print("ArithmeticError")
-except AssertionError:
-    print("AssertionError")
